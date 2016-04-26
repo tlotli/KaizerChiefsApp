ad9ce1ac0b89733586e1bae62cb2233a6a4adad2
tlotli <tlotlimosalo@gmail.com>
2016-04-26 15:23:12 +0200

    First Upload
    First Upload

---
+++ js/TMscroll.js
@@ -0,0 +1,12 @@
+// Javascript
+$(function($) {
+    "use strict";
+
+	/* SMOOTH SCROLL */
+    $('.smooth').click(function(event){		
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 750);
+    });
+
+
+});
